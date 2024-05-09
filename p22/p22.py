9b21a2186ea08b6377a4c5412d305eadd72437ed
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2024-05-09 12:08:53 +0200

    chore: Add POST method to update_python_script route

--- p22/p22.py
+++ p22/p22.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import os
 from flask_cors import CORS
 
@@ -9,12 +9,14 @@ def hello():
     return 'Hello, World!'
 
 
-@app.route('/update_python_script')
+@app.route('/update_python_script', methods=['POST'])
 def update_python_script():
-    for i in range(10):
-        os.system(f'echo "Hello {i}"')
+    user_text = request.form.get('writing_box')
     os.system('/opt/infinigen/blender/blender -b -P /opt/infinigen/testScene.py')
-    return 'Update Python Script!'
+    if user_text:
+        return f'{user_text} Update Python Script!'
+    else:
+        return 'No user text provided.'
 
 if __name__ == '__main__':
     app.run(port=4056)
