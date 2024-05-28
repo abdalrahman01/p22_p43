94f16311261d67d3e9c7e2387c592970172163ff
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2024-05-28 04:18:16 +0200

    fixed cors + add debug info

--- p22/p22.py
+++ p22/p22.py
@@ -4,35 +4,41 @@ from flask_cors import CORS
 import requests
 import subprocess
 
-LLM_HOST = "192.168.0.101" # change with actuall IP for LLM_HOSt
+LLM_HOST = "192.168.0.101" # change with actuall IP for LLM_HOSt # 192.168.0.101
 LLM_PORT = "5000"
 LLM_CHAT = "/chat"
 LLM_NEW_CHAT = "/start_new_chat"
-LLM_RESPONSE_FILE_LOCATION = "/opt/infinigen/testScene.py"
-cors = CORS(app, resources={r"/update_python_script": {"origins": "*"}})
+LLM_RESPONSE_FILE_LOCATION = "/opt/infinigen/testScene.py" # if running locally use th is "/home/atieh/school/p43/infinigen/testScene.py"
+BLENDER_BIN_PATH = "/opt/infinigen/blender/blender" # if running locallay use this "/home/atieh/school/p43/infinigen/blender/blender"  
+
 
 def invoke_command():
-    subprocess.run(["/opt/infinigen/blender/blender", "-b", "-P", "/opt/infinigen/testScene.py"]) 
+    print("running Command: ", [BLENDER_BIN_PATH, "-b", "-P", LLM_RESPONSE_FILE_LOCATION])
+    subprocess.run([BLENDER_BIN_PATH, "-b", "-P", LLM_RESPONSE_FILE_LOCATION]) 
         
 def send_request_to_server(content, host, port):
     payload = {"text", content}
     headers = {'Content-Type': 'application/json'}
     res = requests.get(f"http://{host}:{port}{LLM_CHAT}?text={content}")
+    print("****************\n*Sending Request*\n****************")
     print(res.url)
     print(res.headers)
     print(res.json)
+    print("****************\n*Request Sent*\n****************")
     
     return res
     
 def write_to_file(filepath, content):
+    print("****************\n*Writing to File*\n****************")
     with open(filepath, "w") as g:
         g.write(content)
+    print("****************\n*Written to File*\n****************")
 
 
 
 
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app, resources={r"/update_python_script": {"origins": "*"}})
 @app.route('/')
 def hello():
     return 'Hello, World!'
